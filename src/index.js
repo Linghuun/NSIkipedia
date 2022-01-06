93702f6b23d69f3218eaf200b1c578a9a0a6c869
Dams4K <dams4k@users.noreply.github.com>
2022-01-06 12:07:15 +0100

    new commit by Dams4K

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 function load() {
-    var navSettings = document.getElementById('nav-settings');
-    var navSettingsTitle = document.getElementById('nav-settings-title');
+    var navSettings = document.getElementById("nav-settings");
+    var navSettingsTitle = document.getElementById("nav-settings-title");
 
     navSettingsTitle.addEventListener("click",
         function() {
             navSettings.classList.toggle("is-maximized");
+        }
+    );
+
+    var settingsLanguageSelect = document.getElementById("settings-language-select");
+    settingsLanguageSelect.addEventListener("change",
+        function() {
+            var index = settingsLanguageSelect.selectedIndex;
+            if (index == 0) {
+                window.location.href = "../fr/index.html";
+            }
+            if (index == 1) {
+                window.location.href = "../en/index.html";
+            }
+            console.log(index);
         }
     );
 }
