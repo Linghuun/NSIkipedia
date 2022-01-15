46e1eba04f8e049f85dd00e1c5cdc02f49767a80
Dams4K <dams4k@users.noreply.github.com>
2022-01-15 20:25:48 +0100

    qsd

--- src/test/snake.js
+++ src/test/snake.js
@@ -1,30 +1,54 @@
 class SnakeGame {
     /**
-     * @param {int} start_size  need to be > 1
+     * @param {int} startSize  need to be > 1
      * @param {float} speed     in millisecond
      */
-    constructor(start_size, speed) {
-        this.start_size = start_size;
+    constructor(startSize, speed) {
+        this.start_size = startSize;
         this.speed = speed;
+        this.snake = [];
+        this.snakeGrid = document.getElementById("game_grid");
 
+        for (let i = 0; i < this.startSize; i++) {
+            this.snake.push([i, 0]);
+        }
+
+        this.genGrid();
         document.addEventListener("keydown", this.onKeyDown);
     }
 
-    onKeyDown(event) {
-        if(event.keyCode == 37) {
-            alert('Left was pressed');
+    genGrid() {
+        for (let i = 0; i < 7; i++) {
+            var newRow = document.createElement("div");
+            newRow.classList.add("grid_row");
+            this.snakeGrid.appendChild(newRow);
+
+            for (let j = 0; j < 7; j++) {
+                var newColumn = document.createElement("div");
+                newColumn.classList.add("grid_column")
+                newColumn.innerText = i.toString() + ";" + j.toString();
+                this.newRow.appendChild(newColumn);
+            }
         }
-        else if(event.keyCode == 39) {
-            alert('Right was pressed');
+    }
+
+    onKeyDown(event) {
+        switch (event.key) {
+            case "ArrowDown":
+                alert("arrow down pressed")
+                break;
         }
     }
 
     moveSnake() {
         
     }
+    
+    start() {
+        while (1) {
 
-
+        }
+    }
 }
 
-const snakeGame = new SnakeGame(1);
-alert("qsd")
+const snakeGame = new SnakeGame(1, 5);
