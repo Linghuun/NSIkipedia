e830f605d09062a3c1bb74ecb25eba331b848f1f
Dams4K <dams4k@users.noreply.github.com>
2022-01-19 17:41:01 +0100

    sommaire rajouté

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,24 +1,22 @@
-function load() {
-    var navSettings = document.getElementById("settings");
-    var navSettingsTitle = document.getElementById("settings-title");
+var navSettings = document.getElementById("settings");
+var navSettingsTitle = document.getElementById("settings-title");
 
-    navSettingsTitle.addEventListener("click",
-        function() {
-            navSettings.classList.toggle("is-maximized");
-        }
-    );
+navSettingsTitle.addEventListener("click",
+    function() {
+        navSettings.classList.toggle("is-maximized");
+    }
+);
 
-    var settingsLanguageSelect = document.getElementById("settings-language-select");
-    settingsLanguageSelect.addEventListener("change",
-        function() {
-            var index = settingsLanguageSelect.selectedIndex;
-            if (index == 0) {
-                window.location.href = "../fr/index.html";
-            }
-            if (index == 1) {
-                window.location.href = "../en/index.html";
-            }
-            console.log(index);
+var settingsLanguageSelect = document.getElementById("settings-language-select");
+settingsLanguageSelect.addEventListener("change",
+    function() {
+        var index = settingsLanguageSelect.selectedIndex;
+        if (index == 0) {
+            window.location.href = "../fr/index.html";
         }
-    );
-}
+        if (index == 1) {
+            window.location.href = "../en/index.html";
+        }
+        console.log(index);
+    }
+);
