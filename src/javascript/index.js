7b3ec28a80104e77cde4aa2fb0e642b30b7b3f9a
Dams4K <dams4k@users.noreply.github.com>
2022-01-21 20:09:47 +0100

    j'ai réglé 2/3 trucs

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -10,12 +10,16 @@ navSettingsTitle.addEventListener("click",
 var settingsLanguageSelect = document.getElementById("settings-language-select");
 settingsLanguageSelect.addEventListener("change",
     function() {
+        var url = window.location.href;
+        // alert([window.location.href.lastIndexOf("fr")]);
         var index = settingsLanguageSelect.selectedIndex;
         if (index == 0) {
-            window.location.href = "../fr/index.html";
+            var pos = url.lastIndexOf("en")
+            window.location.href = url.slice(0, pos) + url.slice(pos).replace("en", "fr");
         }
         if (index == 1) {
-            window.location.href = "../en/index.html";
+            var pos = url.lastIndexOf("fr")
+            window.location.href = url.slice(0, pos) + url.slice(pos).replace("fr", "en");
         }
         console.log(index);
     }
