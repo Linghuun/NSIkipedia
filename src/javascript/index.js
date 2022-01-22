5a4392118277f1079b9d381b9b1fa1964ea3effa
Dams4K <dams4k@users.noreply.github.com>
2022-01-22 20:42:12 +0100

    dark mode!!!!!

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -7,6 +7,40 @@ navSettingsTitle.addEventListener("click",
     }
 );
 
+var darkModeButton = document.getElementById("dark_mode");
+darkModeButton.addEventListener("click", 
+    function() {
+        if (darkModeButton.checked) { // dark mode enable
+            document.documentElement.style.setProperty("--body-color", "#373e48ff");
+
+            document.documentElement.style.setProperty("--nav-color", "#1c1f24ff");
+            document.documentElement.style.setProperty("--nav-hover-link", "#373e48ff");
+        
+            document.documentElement.style.setProperty("--settings-color", "#535d6cff");
+            document.documentElement.style.setProperty("--settings-color-hover", "#373e48ff");
+            
+            document.documentElement.style.setProperty("--main-color", "#1c1f24ff");
+            document.documentElement.style.setProperty("--main-text-color", "white");
+
+            document.documentElement.style.setProperty("color", "white");
+        } else { // light mode enable
+            document.documentElement.style.setProperty("--body-color", "#6f7c91ff");
+
+            document.documentElement.style.setProperty("--nav-color", "#373e48ff");
+            document.documentElement.style.setProperty("--nav-hover-link", "#6f7c91ff");
+        
+            document.documentElement.style.setProperty("--settings-color", "#535d6cff");
+            document.documentElement.style.setProperty("--settings-color-hover", "#6f7c91ff");
+            
+            document.documentElement.style.setProperty("--main-color", "#b7bec8ff");
+            document.documentElement.style.setProperty("--main-text-color", "black");
+        
+            document.documentElement.style.setProperty("color", "white");
+        }
+        alert(document.documentElement.style.getPropertyValue("--body-color"));
+    }
+);
+
 var settingsLanguageSelect = document.getElementById("settings-language-select");
 settingsLanguageSelect.addEventListener("change",
     function() {
