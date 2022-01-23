97f50f7e62ab7ec2cab2a85911319643cd27ec6c
Dams4K <dams4k@users.noreply.github.com>
2022-01-23 11:24:24 +0100

    dark mode v2

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,3 +1,17 @@
+var themeUsed = "light";
+
+if (window.location.href.split("?").length > 1) {
+    themeUsed = window.location.href.split("?")[1];
+    setTheme(themeUsed);
+}
+
+function switchTo(pageName) {
+    var url = window.location.href.split("?")[0];
+    let n = window.location.href.split("/").length;
+    url = url.replace(url.split("/")[n-1], "") + pageName;
+    window.location.href = url + "?" + themeUsed;
+}
+
 var navSettings = document.getElementById("settings");
 var navSettingsTitle = document.getElementById("settings-title");
 
@@ -7,45 +21,10 @@ navSettingsTitle.addEventListener("click",
     }
 );
 
-var darkModeButton = document.getElementById("dark_mode");
-darkModeButton.addEventListener("click", 
-    function() {
-        if (darkModeButton.checked) { // dark mode enable
-            document.documentElement.style.setProperty("--body-color", "#373e48ff");
-
-            document.documentElement.style.setProperty("--nav-color", "#1c1f24ff");
-            document.documentElement.style.setProperty("--nav-hover-link", "#373e48ff");
-        
-            document.documentElement.style.setProperty("--settings-color", "#535d6cff");
-            document.documentElement.style.setProperty("--settings-color-hover", "#373e48ff");
-            
-            document.documentElement.style.setProperty("--main-color", "#1c1f24ff");
-            document.documentElement.style.setProperty("--main-text-color", "white");
-
-            document.documentElement.style.setProperty("color", "white");
-        } else { // light mode enable
-            document.documentElement.style.setProperty("--body-color", "#6f7c91ff");
-
-            document.documentElement.style.setProperty("--nav-color", "#373e48ff");
-            document.documentElement.style.setProperty("--nav-hover-link", "#6f7c91ff");
-        
-            document.documentElement.style.setProperty("--settings-color", "#535d6cff");
-            document.documentElement.style.setProperty("--settings-color-hover", "#6f7c91ff");
-            
-            document.documentElement.style.setProperty("--main-color", "#b7bec8ff");
-            document.documentElement.style.setProperty("--main-text-color", "black");
-        
-            document.documentElement.style.setProperty("color", "white");
-        }
-        alert(document.documentElement.style.getPropertyValue("--body-color"));
-    }
-);
-
 var settingsLanguageSelect = document.getElementById("settings-language-select");
 settingsLanguageSelect.addEventListener("change",
     function() {
         var url = window.location.href;
-        // alert([window.location.href.lastIndexOf("fr")]);
         var index = settingsLanguageSelect.selectedIndex;
         if (index == 0) {
             var pos = url.lastIndexOf("en")
@@ -55,6 +34,56 @@ settingsLanguageSelect.addEventListener("change",
             var pos = url.lastIndexOf("fr")
             window.location.href = url.slice(0, pos) + url.slice(pos).replace("fr", "en");
         }
-        console.log(index);
+    }
+);
+
+function setTheme(theme) {
+    if (theme == "light") {
+        document.documentElement.style.setProperty("--body-color", "#6f7c91ff");
+
+        document.documentElement.style.setProperty("--nav-color", "#373e48ff");
+        document.documentElement.style.setProperty("--nav-hover-link", "#6f7c91ff");
+    
+        document.documentElement.style.setProperty("--settings-color", "#535d6cff");
+        document.documentElement.style.setProperty("--settings-color-hover", "#6f7c91ff");
+        
+        document.documentElement.style.setProperty("--main-color", "#b7bec8ff");
+        document.documentElement.style.setProperty("--main-text-color", "black");
+    
+        document.documentElement.style.setProperty("--scrollbar-track-color", "#6f7c91ff");
+        document.documentElement.style.setProperty("--scrollbar-thumb-color", "#373e48ff");
+
+        document.documentElement.style.setProperty("color", "white");
+
+        themeUsed = "light";
+    } else if (theme == "dark") {
+        document.documentElement.style.setProperty("--body-color", "#373e48ff");
+
+        document.documentElement.style.setProperty("--nav-color", "#1c1f24ff");
+        document.documentElement.style.setProperty("--nav-hover-link", "#373e48ff");
+    
+        document.documentElement.style.setProperty("--settings-color", "#535d6cff");
+        document.documentElement.style.setProperty("--settings-color-hover", "#373e48ff");
+        
+        document.documentElement.style.setProperty("--main-color", "#1c1f24ff");
+        document.documentElement.style.setProperty("--main-text-color", "white");
+
+        document.documentElement.style.setProperty("--scrollbar-track-color", "#373e48ff");
+        document.documentElement.style.setProperty("--scrollbar-thumb-color", "#1c1f24ff");
+
+        document.documentElement.style.setProperty("color", "white");
+        
+        themeUsed = "dark";
+    }
+}
+
+var darkModeButton = document.getElementById("dark_mode");
+darkModeButton.addEventListener("click", 
+    function() {
+        if (darkModeButton.checked) {       // dark mode enable
+            setTheme("dark")
+        } else {                            // light mode enable
+            setTheme("light")
+        }
     }
 );
